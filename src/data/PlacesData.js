deab6230826b1e648a191af3da44f103b85dd7e0
Malach Salama <legitdiamonds050@gmail.com>
2022-11-17 12:22:18 +0300

    feat:updated data for managing state

---
+++ src/data/PlacesData.js
@@ -0,0 +1,179 @@
+import s1 from "../images/stay/Stay1.svg";
+import s2 from "../images/stay/Stay2.svg";
+import s3 from "../images/stay/Stay3.svg";
+import s4 from "../images/stay/Stay4.svg";
+import s5 from "../images/stay/Stay5.svg";
+import s6 from "../images/stay/Stay6.svg";
+import s7 from "../images/stay/Stay7.svg";
+import s8 from "../images/stay/Stay8.svg";
+import s9 from "../images/stay/Stay9.svg";
+import s10 from "../images/stay/Stay10.svg";
+import s11 from "../images/stay/Stay11.svg";
+import s12 from "../images/stay/Stay12.svg";
+import s13 from "../images/stay/Stay13.svg";
+import s14 from "../images/stay/Stay14.svg";
+import s15 from "../images/stay/Stay15.svg";
+import s16 from "../images/stay/Stay16.svg";
+
+export const placesData = [
+    {
+        id: 1,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s1,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 2,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s2,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 3,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s3,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 4,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s4,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 5,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s5,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 6,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s6,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 7,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s7,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 8,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s8,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 9,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s9,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 10,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s10,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 11,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s11,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 12,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s12,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 13,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s13,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 14,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s14,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 15,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s15,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    },
+    {
+        id: 16,
+        title: "Desert king",
+        description: "1MBT per night",
+        img: s16,
+        distance: "2345",
+        availability: "2weeks",
+        starRating: 5,
+        liked: false
+    }
+]
